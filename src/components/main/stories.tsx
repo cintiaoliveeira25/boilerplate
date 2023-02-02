d5a4367e11322d4b5c6a6581c13db0fbfcc94e1d
cintiaoliveeira25 <cintia.oliveeira@hotmail.com>
2023-02-02 17:48:39 -0300

    install and config storybook

---
+++ src/components/main/stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Main } from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as ComponentMeta<typeof Main>;
+
+export const Basic: ComponentStory<typeof Main> = (args) => <Main {...args} />;
+Basic.args = {
+  title: 'title default',
+  description: 'description default'
+};
+
+export const Default: ComponentStory<typeof Main> = (args) => (
+  <Main {...args} />
+);
