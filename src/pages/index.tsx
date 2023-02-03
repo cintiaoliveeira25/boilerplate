11904d6e523b15ae9ab59b5c4eed3f8d4137c2db
cintiaoliveeira25 <cintia.oliveeira@hotmail.com>
2023-02-02 21:34:05 -0300

    remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
 import Head from 'next/head';
 import { Main } from '../components/main';
 
-const foo = 'bla';
-
-type Props = {
-  title: string;
-};
-
 export default function Home() {
   return (
     <>
